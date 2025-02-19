1f39371dfc6f5975573e22c14cc03ccee2d140ce
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2025-02-19 13:59:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from duckassist import DuckDuckAssist
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 
 load_dotenv()
 
@@ -24,6 +25,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class IPWhitelistMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.headers.get("X-Forwarded-For", request.client.host)
+        client_ip = request.headers.get("CF-Connecting-IP", client_ip) 
+        if client_ip not in ALLOWED_IPS:
+            print("Blocked ip request: ",client_ip)
+            raise HTTPException(status_code=403, detail="Access forbidden")
+        return await call_next(request)
+
+app.add_middleware(IPWhitelistMiddleware)
+
+"""
 @app.middleware("http")
 async def ip_whitelist_middleware(request: Request, call_next):
     forwarded_for = request.headers.get("X-Forwarded-For")
@@ -32,7 +45,8 @@ async def ip_whitelist_middleware(request: Request, call_next):
     if client_ip not in ALLOWED_IPS:
         raise HTTPException(status_code=403, detail="Access forbidden")
     return await call_next(request)
-    
+"""
+
 @app.get("/v1/get-token")
 async def getToken():
     try:
